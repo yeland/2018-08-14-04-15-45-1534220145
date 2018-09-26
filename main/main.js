cf43b6a47a6f984158b79ece685317ad97c37697
yeland <wlx100715@163.com>
2018-09-26 22:21:33 +0800

    main

--- main/main.js
+++ main/main.js
@@ -1,4 +1,11 @@
 // Write your cade below:
 module.exports = function main() {
+  if(arguments.length===0){
+	  return 0;
+  }
   
+  var x = arguments[0];
+  var y = arguments[1];
+  
+  return x%y;
 };
